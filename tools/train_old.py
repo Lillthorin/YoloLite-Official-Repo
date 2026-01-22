3d39a1d26e8277de69981daec721f3d8b7b003db
Lillthorin <thorin.mathias@gmail.com>
2026-01-22 12:43:36 +0100

    Update train_old.py

--- tools/train_old.py
+++ tools/train_old.py
@@ -14,9 +14,9 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 from torch.utils.data import DataLoader
 from scripts.model.model_v2 import YOLOLiteMS, YOLOLiteMS_CPU
-from scripts.data.dataset import YoloDataset
+from scripts.data.dataset_old import YoloDataset
 from scripts.data.augment import get_base_transform, get_val_transform
-from scripts.loss.loss import LossAF
+from scripts.loss.loss_old import LossAF
 from scripts.helpers.sanity_check import visualize_batch
 from scripts.helpers.schedulers import build_scheduler
 from scripts.helpers.helpers import yolo_collate, _coco_eval_from_lists,  set_seed, save_val_debug_anchorfree, _decode_batch_to_coco_dets, _xyxy_to_xywh, _write_json_atomic, _append_csv
@@ -554,6 +554,7 @@ if __name__ == "__main__":
 
     
     
+
 
 
 
